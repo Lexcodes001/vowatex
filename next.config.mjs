08cb2686bbdb27d78456524099e44fcaf235b592
Lexcodes001 <adetayoalexander12@gmail.com>
2024-05-25 21:29:27 +0100

    testing route error solution

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,21 @@
 const nextConfig = {
   output: "export",
   distDir: "out",
+  // reactStrictMode: false,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/about": { page: "/about" },
+  //   };
+  // },
 };
 
 export default nextConfig;
