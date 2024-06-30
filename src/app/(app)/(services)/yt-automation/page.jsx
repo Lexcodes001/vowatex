11d7cb1bb838a15792aebfa7db4ab803cbebe20c
Lexcodes001 <adetayoalexander12@gmail.com>
2024-06-30 22:35:35 +0100

    services error resolved

--- src/app/(app)/(services)/yt-automation/page.jsx
+++ src/app/(app)/(services)/yt-automation/page.jsx
@@ -100,7 +100,7 @@ const Page = () => {
           <header>
             Contact us to set up your automation solutions consultation
           </header>
-          <Form formType={client}/>
+          <Form formType={'client'}/>
         </div>
       </section>
 
